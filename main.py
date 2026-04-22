4e075d0344fb9c4135a5cb32ad7d9d048aa2ab3d
shrys <shreyasjain2018@gmail.com>
2026-04-22 18:12:37 +0000

    modify hyperparameters, fix dropout integration

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ from torch.nn import functional as F
 DATA_URL = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
 DATA_PATH = "input.txt"
 
-batch_size = 32
-block_size = 8
-n_embd = 32
-n_head = 4
-n_layer = 1
+batch_size = 64
+block_size = 256
+n_embd = 384
+n_head = 6
+n_layer = 6
 dropout = 0.2
 max_iters = 10000
-eval_interval = 300
+eval_interval = 500
 eval_iters = 200
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 torch.manual_seed(42)
